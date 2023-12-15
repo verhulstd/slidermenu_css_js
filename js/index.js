805c0cf244cee4ca5a1a0aac82b0200f0ab20457
David Verhulst <info@inventivemedia.be>
2023-12-15 12:04:42 +0100

    slidermenu done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 import "../css/style.scss";
 
-console.log("javascript works in public area....");
+// console.log("javascript works in public area....");
+
+const navRef = document.querySelector("nav");
+const divRef = document.querySelector("header > div");
+
+divRef.onclick = function () {
+  navRef.classList.toggle("open");
+};
